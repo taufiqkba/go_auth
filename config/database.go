3f403a2080e8888300b085b2240e96b0ccae3b72
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-06-26 16:39:18 +0700

    implement db connection

--- config/database.go
+++ config/database.go
@@ -3,5 +3,11 @@ package config
 import "database/sql"
 
 func DB() (db *sql.DB, err error) {
-	
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPassword := "root"
+	dbName := "go_auth"
+
+	db, err = sql.Open(dbDriver, dbUser+":"+dbPassword+"@/"+dbName)
+	return
 }
