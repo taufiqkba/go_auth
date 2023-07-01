9e52caa6666a24a15b0c6fcd8e1754d625bbd344
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-07-01 23:55:31 +0700

    setup label for error message

--- entities/user.go
+++ entities/user.go
@@ -2,9 +2,9 @@ package entities
 
 type User struct {
 	Id        int64
-	Name      string
-	Email     string
-	Username  string
-	Password  string
-	CPassword string
+	Name      string `validate:"required" label:"Full Name"`
+	Email     string `validate:"required,email"`
+	Username  string `validate:"required,gte=3"`
+	Password  string `validate:"required,gte=6"`
+	CPassword string `validate:"required,eqfield=Password" label:"Password Confirmation"`
 }
