078c3b3dd6c999ebb3d888d8fa9370b2e0a6496f
taufiqkba <taufiqkurniawan9991@gmail.com>
2023-07-02 23:57:56 +0700

    Create custome error message for register field

--- controllers/authcontroller.go
+++ controllers/authcontroller.go
@@ -2,12 +2,18 @@ package controllers
 
 import (
 	"errors"
+	"net/http"
+	"reflect"
+	"text/template"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en2 "github.com/go-playground/validator/v10/translations/en"
 	"github.com/taufiqkba/go_auth/config"
 	"github.com/taufiqkba/go_auth/entities"
 	"github.com/taufiqkba/go_auth/models"
 	"golang.org/x/crypto/bcrypt"
-	"net/http"
-	"text/template"
 )
 
 type UserInput struct {
@@ -140,59 +146,105 @@ func Register(w http.ResponseWriter, r *http.Request) {
 			CPassword: r.Form.Get("c_password"),
 		}
 
-		errorMessage := make(map[string]interface{})
-		if user.Name == "" {
-			errorMessage["Name"] = "Name can't empty"
+		//errorMessage := make(map[string]interface{})
+		//if user.Name == "" {
+		//	errorMessage["Name"] = "Name can't empty"
+		//}
+		//if user.Email == "" {
+		//	errorMessage["Email"] = "Email can't empty"
+		//}
+		//if user.Username == "" {
+		//	errorMessage["Username"] = "Username can't empty"
+		//}
+		//if user.Password == "" {
+		//	errorMessage["Password"] = "Password can't empty"
+		//}
+		//if user.CPassword == "" {
+		//	errorMessage["CPassword"] = "Confirmation Password can't empty"
+		//} else {
+		//	if user.CPassword != user.Password {
+		//		errorMessage["CPassword"] = "Confirmation Password not match"
+		//	}
+		//}
+		//if len(errorMessage) > 0 {
+		//	//	validation form failes
+		//	data := map[string]interface{}{
+		//		"validation": errorMessage,
+		//	}
+		//	temp, err := template.ParseFiles("views/register.gohtml")
+		//	err = temp.Execute(w, data)
+		//	if err != nil {
+		//		return
+		//	}
+		//} else {
+		//	//	hash password using bcrypt
+		//	hashPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		//	if err != nil {
+		//		return
+		//	}
+		//	user.Password = string(hashPassword) //return back to entities
+		//
+		//	//	insert to database
+		//	_, err = UserModel.Create(user)
+		//	var message string
+		//	if err != nil {
+		//		message = "Register failed: " + message
+		//	} else {
+		//		message = "Registration success, click login"
+		//	}
+		//
+		//	data := map[string]interface{}{
+		//		"message": message,
+		//	}
+		//
+		//	temp, _ := template.ParseFiles("views/register.gohtml")
+		//	err = temp.Execute(w, data)
+		//	if err != nil {
+		//		return
+		//	}
+		//}
+
+		//translator error
+		translator := en.New()
+		uni := ut.New(translator, translator)
+		trans, _ := uni.GetTranslator("en")
+
+		//	validation using go playground
+		validate := validator.New()
+
+		//register default translation english
+		err = en2.RegisterDefaultTranslations(validate, trans)
+		if err != nil {
+			return
 		}
-		if user.Email == "" {
-			errorMessage["Email"] = "Email can't empty"
-		}
-		if user.Username == "" {
-			errorMessage["Username"] = "Username can't empty"
-		}
-		if user.Password == "" {
-			errorMessage["Password"] = "Password can't empty"
-		}
-		if user.CPassword == "" {
-			errorMessage["CPassword"] = "Confirmation Password can't empty"
-		} else {
-			if user.CPassword != user.Password {
-				errorMessage["CPassword"] = "Confirmation Password not match"
+
+		//change default label error
+		validate.RegisterTagNameFunc(func(field reflect.StructField) string {
+			labelName := field.Tag.Get("label")
+			return labelName
+		})
+
+		validate.RegisterTranslation("required", trans, func(ut ut.Translator) error {
+			return ut.Add("required", "{0} can't empty", true)
+		}, func(ut ut.Translator, fe validator.FieldError) string {
+			t, _ := ut.T("required", fe.Field())
+			return t
+		})
+
+		//validation process
+		vErrors := validate.Struct(user)
+
+		errorMessages := make(map[string]interface{})
+		if vErrors != nil {
+			for _, e := range vErrors.(validator.ValidationErrors) {
+				errorMessages[e.StructField()] = e.Translate(trans)
 			}
-		}
-		if len(errorMessage) > 0 {
-			//	validation form failes
 			data := map[string]interface{}{
-				"validation": errorMessage,
+				"validation": errorMessages,
+				"user":       user,
 			}
-			temp, err := template.ParseFiles("views/register.gohtml")
-			err = temp.Execute(w, data)
-			if err != nil {
-				return
-			}
-		} else {
-			//	hash password using bcrypt
-			hashPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-			if err != nil {
-				return
-			}
-			user.Password = string(hashPassword) //return back to entities
-
-			//	insert to database
-			_, err = UserModel.Create(user)
-			var message string
-			if err != nil {
-				message = "Register failed: " + message
-			} else {
-				message = "Registration success, click login"
-			}
-
-			data := map[string]interface{}{
-				"message": message,
-			}
-
 			temp, _ := template.ParseFiles("views/register.gohtml")
-			err = temp.Execute(w, data)
+			err := temp.Execute(w, data)
 			if err != nil {
 				return
 			}
